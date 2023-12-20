1805ff02f0394927b43185f8831f1dbc23c2aff3
ErnestoBFigueroa <Ingenes@INGENCOTIMO21.ingenes.local>
2023-12-20 01:50:31 -0600

    postgres added

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import express from 'express'
+import express from "express";
+import { config } from "dotenv";
+import pg from "pg";
 
-const app = express()
+config();
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
+const app = express();
 
-app.listen(3000)
-console.log('Server on port', 3000)
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query(
+    "SELECT * FROM public.projects ORDER BY id ASC"
+  );
+  return res.json(result.rows[0]);
+});
+
+app.listen(3000);
+console.log("Server on port", 3000);
