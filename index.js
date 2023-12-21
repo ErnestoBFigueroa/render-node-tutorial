c444b4bd223ef30a730d053c5e838910b85e95fa
ErnestoBFigueroa <Ingenes@INGENCOTIMO21.ingenes.local>
2023-12-21 08:43:00 -0600

    Added cors

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 import express from "express";
 import { config } from "dotenv";
 import pg from "pg";
+import cors from "cors";
 
 config();
 
 const app = express();
+const whiteList = [process.env.FRONTEND_URL];
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
+app.use(
+  cors({
+    origin: whiteList,
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/ping", async (req, res) => {
+app.get("/projects", async (req, res) => {
   const result = await pool.query(
     "SELECT * FROM public.projects ORDER BY id ASC"
   );
